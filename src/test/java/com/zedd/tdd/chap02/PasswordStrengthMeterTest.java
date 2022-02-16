6ec5958f27519ad7e21fafeefb5909a9aa33de63
zedd.c <zedd.c@kakaocorp.com>
2022-02-16 18:54:31 +0900

    비밀번호 강도 측정 기능 개발 - tdd #09 refactor - testCode 리팩토링 - 중복된 코드 합치기

--- src/test/java/com/zedd/tdd/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/com/zedd/tdd/chap02/PasswordStrengthMeterTest.java
@@ -8,21 +8,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PasswordStrengthMeterTest {
     PasswordStrengthMeter meter = new PasswordStrengthMeter();
 
-    @Test
-    void 암호가_모든_조건을_충족하면_암호_강도는_강함() {
-        PasswordStrength result = meter.check("abcdef!@A12");
-        assertEquals(PasswordStrength.STRONG, result);
+    private void assertStrength(String s, PasswordStrength strong) {
+        PasswordStrength result = meter.check(s);
+        assertEquals(strong, result);
     }
 
     @Test
+    void 암호가_모든_조건을_충족하면_암호_강도는_강함() {
+        assertStrength("abcdef!@A12", PasswordStrength.STRONG);
+    }
+
+
+    @Test
     void 암호가_8글자_미만_조건을_제외한_모든_조건을_충족하면_암호_강도는_보통() {
-        PasswordStrength result = meter.check("abcd@A1");
-        assertEquals(PasswordStrength.NORMAL, result);
+        assertStrength("abcd@A1", PasswordStrength.NORMAL);
     }
 
     @Test
     void 암호가_숫자를_포함하지_않는_조건을_제외한_모든_조건을_충족하면_암호_강도는_보통() {
-        PasswordStrength result = meter.check("abcasdAasdsad");
-        assertEquals(PasswordStrength.NORMAL, result);
+        assertStrength("abcasdAasdsad", PasswordStrength.NORMAL);
     }
 }
