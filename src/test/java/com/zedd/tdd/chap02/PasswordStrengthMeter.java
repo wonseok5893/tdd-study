e6465dcf5279c0b45e50bba8b714e336f1470f1c
zedd.c <zedd.c@kakaocorp.com>
2022-02-16 18:47:43 +0900

    비밀번호 강도 측정 기능 개발 - tdd #07 fail to success

--- src/test/java/com/zedd/tdd/chap02/PasswordStrengthMeter.java
+++ src/test/java/com/zedd/tdd/chap02/PasswordStrengthMeter.java
@@ -4,6 +4,14 @@ public class PasswordStrengthMeter {
     public PasswordStrength check(String password) {
         if (password.length() < 8)
             return PasswordStrength.NORMAL;
+        boolean containsNum = false;
+        for (char c : password.toCharArray()) {
+            if (Character.isDigit(c)) {
+                containsNum = true;
+                break;
+            }
+        }
+        if (!containsNum) return PasswordStrength.NORMAL;
         return PasswordStrength.STRONG;
     }
 }
