c879c18c05950130123610b5298b55ddd2702510
zedd.c <zedd.c@kakaocorp.com>
2022-02-17 15:35:43 +0900

    비밀번호 강도 측정 기능 개발 - tdd #25 - refactor

--- src/test/java/com/zedd/tdd/chap02/PasswordStrengthMeter.java
+++ src/test/java/com/zedd/tdd/chap02/PasswordStrengthMeter.java
@@ -3,15 +3,18 @@ package com.zedd.tdd.chap02;
 public class PasswordStrengthMeter {
     public PasswordStrength check(String password) {
         if (password == null || password.isEmpty()) return PasswordStrength.INVALID;
-        int metCount = 0;
-
-        if (password.length() >= 8) metCount++;
-        if (isContainsNum(password)) metCount++;
-        if (isContainsUpper(password)) metCount++;
-
+        int metCount = getMetCriteriaCounts(password);
         if (metCount <= 1) return PasswordStrength.WEEK;
         if (metCount == 2) return PasswordStrength.NORMAL;
         return PasswordStrength.STRONG;
+    }
+
+    private int getMetCriteriaCounts(String password) {
+        int metCount = 0;
+        if (password.length() >= 8) metCount++;
+        if (isContainsNum(password)) metCount++;
+        if (isContainsUpper(password)) metCount++;
+        return metCount;
     }
 
     private boolean isContainsUpper(String password) {
