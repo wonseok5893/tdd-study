47a4672dcaf533a59132090c71c1f5a96799b61d
zedd.c <zedd.c@kakaocorp.com>
2022-02-18 11:04:45 +0900

    zedd 테스트 작성 순서 연습 - #08 test 추가 fail to successs

--- src/test/java/com/zedd/tdd/chap03/ExpirationDeterminer.java
+++ src/test/java/com/zedd/tdd/chap03/ExpirationDeterminer.java
@@ -5,8 +5,8 @@ import java.time.LocalDateTime;
 public class ExpirationDeterminer {
 
     public LocalDateTime determine(LocalDateTime before, int pay) {
-        if (pay == 100000)
-            return before.plusYears(1);
+        if (pay >= 100000)
+            return before.plusYears(pay / 100000);
 
         return before.plusMonths(pay / 10000);
     }
