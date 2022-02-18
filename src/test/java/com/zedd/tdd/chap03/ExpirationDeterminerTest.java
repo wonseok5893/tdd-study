5ba55895a5d92651e50d42f3b0f597efd1a36fc7
zedd.c <zedd.c@kakaocorp.com>
2022-02-18 11:02:10 +0900

    zedd 테스트 작성 순서 연습 - #06 refactor

--- src/test/java/com/zedd/tdd/chap03/ExpirationDeterminerTest.java
+++ src/test/java/com/zedd/tdd/chap03/ExpirationDeterminerTest.java
@@ -7,8 +7,9 @@ import java.time.LocalDateTime;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExpirationDeterminerTest {
+    LocalDateTime beforeDateTime = LocalDateTime.now();
 
-    private void assertExpireDate(LocalDateTime beforeDateTime, int pay, LocalDateTime expectedDateTime) {
+    private void assertExpireDate(int pay, LocalDateTime expectedDateTime) {
         ExpirationDeterminer determiner = new ExpirationDeterminer();
         LocalDateTime newExpiredDate = determiner.determine(beforeDateTime, pay);
         assertEquals(newExpiredDate, expectedDateTime);
@@ -16,20 +17,17 @@ public class ExpirationDeterminerTest {
 
     @Test
     void 십만원을_납부하면_서비스를_1년_제공() {
-        LocalDateTime beforeDateTime = LocalDateTime.now();
-        assertExpireDate(beforeDateTime, 100000, beforeDateTime.plusYears(1));
+        assertExpireDate(100000, beforeDateTime.plusYears(1));
     }
 
     @Test
     void 만원을_납부하면_서비스를_1달_제공() {
-        LocalDateTime beforeDateTime = LocalDateTime.now();
-        assertExpireDate(beforeDateTime, 10000, beforeDateTime.plusMonths(1));
+        assertExpireDate(10000, beforeDateTime.plusMonths(1));
     }
 
     @Test
     void 두달이상_요금을_납부할_수_있다() {
-        LocalDateTime beforeDateTime = LocalDateTime.now();
-        assertExpireDate(beforeDateTime, 20000, beforeDateTime.plusMonths(2));
-        assertExpireDate(beforeDateTime, 30000, beforeDateTime.plusMonths(3));
+        assertExpireDate(20000, beforeDateTime.plusMonths(2));
+        assertExpireDate(30000, beforeDateTime.plusMonths(3));
     }
 }
